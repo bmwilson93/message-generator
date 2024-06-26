c80d84768d3c0dfe5b4ad65aab80bda9e62e757d
Brian Wilson <bmwilson93@gmail.com>
2024-06-26 01:49:24 -0600

    added result string and display

--- script.js
+++ script.js
@@ -3,14 +3,17 @@
 
 // Game Idea Generator
 
-const subjects = [];
-const actions = [];
-const reasons = [];
+const subjects = ['A squirrel', 'A car', 'Several chipmunks', 'A robot named Bob', 'A forgotten spirit'];
+const actions = ['travels to the ends of the Earth', 'fights through a horde of zombies', 'learns to do ninja stuff', 'practives backflips', 'drive a racecar'];
+const reasons = ['to stop an evil robot', 'to learn the power of friendship', 'to start a new life on a farm', 'to save their dad', 'to eat a really big burrito'];
 
 // generate a number between 0 - array.length (for each array)
 let subject  = Math.floor(Math.random() * subjects.length);
 let action  = Math.floor(Math.random() * actions.length);
-let resons  = Math.floor(Math.random() * reasons.length);
+let reason  = Math.floor(Math.random() * reasons.length);
 
 // build out the result string
+let result = `${subject} ${action} ${reason}.`;
+
 // display
+console.log(result);
