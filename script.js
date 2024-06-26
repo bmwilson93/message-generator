efe7a43af44231cd703276e3129b3e1a8cb4d3c7
Brian Wilson <bmwilson93@gmail.com>
2024-06-26 01:35:29 -0600

    generate random number for each component

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ const subjects = [];
 const actions = [];
 const reasons = [];
 
+// generate a number between 0 - array.length (for each array)
+let subject  = Math.floor(Math.random() * subjects.length);
+let action  = Math.floor(Math.random() * actions.length);
+let resons  = Math.floor(Math.random() * reasons.length);
+
+// build out the result string
+// display
